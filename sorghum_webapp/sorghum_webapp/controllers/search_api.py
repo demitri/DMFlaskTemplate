618a5bc58856f7e07d23d1b7b3e114d0b7c93387
Andrew Olson <olson@cshl.edu>
2018-06-19 00:16:43 -0400

    update to search

--- sorghum_webapp/sorghum_webapp/controllers/search_api.py
+++ sorghum_webapp/sorghum_webapp/controllers/search_api.py
@@ -17,29 +17,13 @@ def search_api(cat):
     q = valueFromRequest(key="q", request=request)
     if cat in WP_CATS:
         with requests.Session() as session:
-            url = WP_BASE_URL + cat + '?context=embed&search=' + q
+            url = WP_BASE_URL + cat + '?context=embed&_embed=true&search=' + q
             response = session.get(url=url)
             dict = {}
             dict['docs'] = response.json()
             dict['numFound'] = int(response.headers['X-WP-TOTAL'])
             results = jsonify(dict)
-    elif cat == 'Sorghumbase':
-        with requests.Session() as session:
-            dict = {}
-            dict['numFound'] = 0
-            dict['categories'] = {}
-            for cat in WP_CATS:
-                url = WP_BASE_URL + cat + '?context=embed&_embed=true&search=' + q
-                response = session.get(url=url)
-                dict['numFound'] += int(response.headers['X-WP-TOTAL'])
-                dict['categories'][cat] = {}
-                dict['categories'][cat]['numFound'] = int(response.headers['X-WP-TOTAL'])
-                dict['categories'][cat]['docs'] = response.json();
-            results = jsonify(dict)
-    elif cat == 'Gramene':
-        with requests.Session() as session:
-            url = 'http://data.gramene.org/search?q=' + q
-            response = session.get(url=url)
-            results = jsonify(response.json()['response'])
+    else:
+        results = jsonify(WP_CATS)
     results.headers.add('Access-Control-Allow-Origin','*')
     return results
