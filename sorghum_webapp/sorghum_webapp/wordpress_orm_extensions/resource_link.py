a203de6dd2b75005b0e6391d9b9d30868fd7240a
Ivar Meijs <meijs@cumberland.cshl.edu>
2018-06-26 14:56:02 -0400

    Added News to navbar, simplified jobs and resource links.

--- sorghum_webapp/sorghum_webapp/wordpress_orm_extensions/resource_link.py
+++ sorghum_webapp/sorghum_webapp/wordpress_orm_extensions/resource_link.py
@@ -15,15 +15,15 @@ from wordpress_orm import WPEntity, WPRequest, WPORMCacheObjectNotFoundError
 logger = logging.getLogger("wordpress_orm")
 
 class ResourceLink(WPEntity):
-	
+
 	def __init__(self, id=None, api=None):
 		super().__init__(api=api)
-		
+
 		# related objects that need to be cached
 		self._author = None
 		self._category = None
 		self._resource_image = None
-	
+
 	def __repr__(self):
 		if len(self.s.resource_url) < 11:
 			truncated_url = self.s.resource_url
@@ -37,9 +37,8 @@ class ResourceLink(WPEntity):
 	def schema_fields(self):
 		return ["id", "date", "date_gmt", "guid", "modified", "modified_gmt",
 				"slug", "status", "type", "link", "title", "content", "template",
-				"resource_url", "resource_blurb", "resource_name", "category",
-				"resource_author", "resource_image"]
-	
+				"resource_url", "resource_image"]
+
 	@property
 	def categories(self):
 		'''
@@ -53,7 +52,7 @@ class ResourceLink(WPEntity):
 				except exc.NoEntityFound:
 					logger.debug("Expected to find category ID={0} from post (ID={1}), but no category found.".format(category_id, self.s.id))
 		return self._categories
-	
+
 	@property
 	def category_names(self):
 		return [x.s.name for x in self.categories]
@@ -72,7 +71,7 @@ class ResourceLink(WPEntity):
 			else:
 				self._resource_image = None
 		return self._resource_image
-	
+
 	@property
 	def author(self):
 		'''
@@ -87,41 +86,41 @@ class ResourceLink(WPEntity):
 			else:
 				raise exc.UserNotFound("User ID '{0}' not found.".format(self.author))
 		return self._author
-		
-		
+
+
 class ResourceLinkRequest(WPRequest):
 	'''
 	A class that encapsulates requests for WordPress resource links.
 	'''
-	def __init__(self, api=None):		
+	def __init__(self, api=None):
 		super().__init__(api=api)
 		self.id = None # WordPress ID
 		self._before = None
 		self._after = None
-		
+
 		self._status = list()
 		self._category_ids = list()
 		self._slugs = list()
-	
+
 	@property
 	def parameter_names(self):
 		return ["slug", "before", "after", "status", "categories", "resource_image"]
-	
+
 	def get(self):
 		'''
 		Returns a list of 'Resource Links' objects that match the parameters set in this object.
 		'''
 		self.url = self.api.base_url + "resource-link"
-		
+
 		if self.id:
 			self.url += "/{}".format(self.id)
-		
+
 		# -------------------
 		# populate parameters
 		# -------------------
 		if self.slug:
 			self.parameters["slug"] = self.slug
-			
+
 		if self.before:
 			self.parameters["before"] = self._before.isoformat()
 
@@ -142,11 +141,11 @@ class ResourceLinkRequest(WPRequest):
 				return None
 
 		links_data = self.response.json()
-		
+
 		if isinstance(links_data, dict):
 			# only one object was returned; make it a list
 			links_data = [links_data]
-		
+
 		links = list()
 		for d in links_data:
 
@@ -161,7 +160,7 @@ class ResourceLinkRequest(WPRequest):
 
 			link = ResourceLink(api=self.api)
 			link.json = d
-			
+
 			link.s.id = d["id"]
 			link.s.date = d["date"]
 			link.s.date_gmt = d["date_gmt"]
@@ -176,28 +175,24 @@ class ResourceLinkRequest(WPRequest):
 			link.s.content = d["content"]
 			link.s.template = d["template"]
 			link.s.resource_url = d["resource_url"]
-			link.s.resource_blurb = d["resource_blurb"]
-			link.s.resource_name = d["resource_name"]
-			link.s.category = d["category"]
-			link.s.resource_author = d["resource_author"]
 			link.s.resource_image = d["resource_image"]
-		
+
 			# add to cache
 			self.api.wordpress_object_cache.set(class_name=ResourceLink.__name__, key=link.s.id, value = link)
 			self.api.wordpress_object_cache.set(class_name=ResourceLink.__name__, key=link.s.slug, value = link)
 
 			links.append(link)
-			
+
 		return links
 
-	
+
 	@property
 	def slugs(self):
 		'''
 		The list of post slugs to retrieve.
 		'''
 		return self._slugs
-		
+
 	@slugs.setter
 	def slugs(self, values):
 		if values is None:
@@ -206,7 +201,7 @@ class ResourceLinkRequest(WPRequest):
 			return
 		elif not isinstance(values, list):
 			raise ValueError("Slugs must be provided as a list (or append to the existing list).")
-		
+
 		for s in values:
 			if isinstance(s, str):
 				self._slugs.append(s)
@@ -219,7 +214,7 @@ class ResourceLinkRequest(WPRequest):
 		WordPress parameter to return posts after this date.
 		'''
 		return self._after
-	
+
 	@after.setter
 	def after(self, value):
 		'''
@@ -242,7 +237,7 @@ class ResourceLinkRequest(WPRequest):
 		WordPress parameter to return posts before this date.
 		'''
 		return self._after
-	
+
 	@after.setter
 	def before(self, value):
 		'''
@@ -267,7 +262,7 @@ class ResourceLinkRequest(WPRequest):
 	def categories(self, values):
 		'''
 		This method validates the categories passed to this request.
-		
+
 		It accepts category ID (integer or string) or the slug value.
 		'''
 		if values is None:
@@ -276,7 +271,7 @@ class ResourceLinkRequest(WPRequest):
 			return
 		elif not isinstance(values, list):
 			raise ValueError("Categories must be provided as a list (or append to the existing list).")
-		
+
 		for c in values:
 			cat_id = None
 			if isinstance(c, Category):
@@ -298,7 +293,7 @@ class ResourceLinkRequest(WPRequest):
 						#self._category_ids.append(category.s.id)
 					except exc.NoEntityFound:
 						logger.debug("Asked to find a category with the slug '{0}' but not found.".format(slug))
-			
+
 			# Categories are stored as string ID values.
 			#
 			self._category_ids.append(str(cat_id))
