f7cca8b09d036db956dffe5105d607c8101dcc3c
Andrew Olson <olson@cshl.edu>
2018-06-28 11:59:01 -0400

    update search since refactoring pods

--- sorghum_webapp/sorghum_webapp/controllers/search_api.py
+++ sorghum_webapp/sorghum_webapp/controllers/search_api.py
@@ -19,7 +19,7 @@ def searchapi(cat):
     q = valueFromRequest(key="q", request=request)
     if cat in WP_CATS:
         with requests.Session() as session:
-            url = WP_BASE_URL + cat + '?context=embed&_embed=true&search=' + q
+            url = WP_BASE_URL + cat + '?_embed=true&search=' + q
             if cat == 'posts':
                 url = url + '&categories_exclude=17'
             response = session.get(url=url)
