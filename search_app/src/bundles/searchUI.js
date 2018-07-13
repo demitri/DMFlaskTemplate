c4e3998f0fcff92b378e8938ea49d00d5c2a5fee
Andrew Olson <olson@cshl.edu>
2018-07-13 16:56:37 -0400

    adding hooks for show more / less search results of a given type

--- search_app/src/bundles/searchUI.js
+++ search_app/src/bundles/searchUI.js
@@ -21,7 +21,16 @@ const UIbundle = {
       Genes: true,
       Domains: false,
       Pathways: false,
-      Species: false
+      Species: false,
+      rows: {
+        Posts: 2,
+        Events: 2,
+        Jobs: 2,
+        People: 2,
+        Links: 2,
+        Papers: 2,
+        Genes: 20
+      }
     };
     return (state = initialState, {type, payload}) => {
       if (type === 'CATEGORY_TOGGLED') {
@@ -31,6 +40,14 @@ const UIbundle = {
         update[payload] = !state[payload];
         return Object.assign(state, update)
       }
+      if (type === 'CATEGORY_QUANTITY_CHANGED') {
+        let update = {
+          updates: state.updates + 1,
+          rows: {}
+        };
+        update.rows[payload.cat] = state.rows[payload.cat] + payload.delta;
+        return Object.assign(state, update)
+      }
       return state
     }
   },
@@ -38,6 +55,16 @@ const UIbundle = {
     dispatch({type: 'CATEGORY_TOGGLED', payload: cat})
   },
   persistActions: ['CATEGORY_TOGGLED'],
+  doChangeQuantity: (cat, delta) => ({dispatch}) => {
+    dispatch({type: 'CATEGORY_QUANTITY_CHANGED', payload: {cat:cat, delta:delta}});
+    if (cat === 'Genes') dispatch({type: 'GRAMENE_GENES_CLEARED'});
+    if (cat === 'Posts') dispatch({type: 'SORGHUM_POSTS_CLEARED'});
+    if (cat === 'Links') dispatch({type: 'SORGHUM_LINKS_CLEARED'});
+    if (cat === 'Events') dispatch({type: 'SORGHUM_EVENTS_CLEARED'});
+    if (cat === 'Jobs') dispatch({type: 'SORGHUM_JOBS_CLEARED'});
+    if (cat === 'People') dispatch({type: 'SORGHUM_PEOPLE_CLEARED'});
+    if (cat === 'Papers') dispatch({type: 'SORGHUM_PAPERS_CLEARED'});
+  },
   doUpdateTheQueries: query => ({dispatch, getState}) => {
     const url = new URL(getState().url.url);
     url.search = ensureString(query);
@@ -59,7 +86,7 @@ const UIbundle = {
   },
   selectSearchUI: state => state.searchUI,
   selectSearchUpdated: state => state.searchUI.updates,
-  selectShowGramene: state => state.searchUI.showGramene
+  selectRows: state=> state.searchUI.rows
 };
 
 export default UIbundle;
