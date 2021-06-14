a9c0f782a8c5db9a359169db88d6a97dcbef714c
Andrew Olson <olson@cshl.edu>
2021-06-14 18:56:11 -0400

    if no gravatar, use faces

--- sorghum_webapp/sorghum_webapp/controllers/people.py
+++ sorghum_webapp/sorghum_webapp/controllers/people.py
@@ -20,49 +20,60 @@ logger = logging.getLogger("wordpress_orm")
 
 people_page = flask.Blueprint("people_page", __name__)
 
+def chooseFace(wpapi,users):
+    for u in users:
+        if wpapi.media(slug=u.s.username) is None:
+            u.imgURL = u.s.avatar_urls['96']
+        else:
+            media = wpapi.media(slug=u.s.username)
+            u.imgURL = media.s.source_url
+
 @people_page.route('/people')
 def people():
-	''' People page. '''
-	templateDict = navbar_template('Community')
+    ''' People page. '''
+    templateDict = navbar_template('Community')
 
-	with wpapi.Session():
+    with wpapi.Session():
 
-		team_request = wpapi.UserRequest()
-		team_request.context = "edit"
-		team_request.per_page = 50
-		team_request.roles = ['team_member']
-		team = team_request.get(class_object=SBUser)
+        team_request = wpapi.UserRequest()
+        team_request.context = "edit"
+        team_request.per_page = 50
+        team_request.roles = ['team_member']
+        team = team_request.get(class_object=SBUser)
+        chooseFace(wpapi,team)
 
-		leader = [u for u in team if ("Ware" in u.s.name)]
-		team = [u for u in team if not ("Ware" in u.s.name)]
+        leader = [u for u in team if ("Ware" in u.s.name)]
+        team = [u for u in team if not ("Ware" in u.s.name)]
 
-		esc_request = wpapi.UserRequest()
-		esc_request.context = "edit"
-		esc_request.per_page = 50
-		esc_request.roles = ['former_team_member']
-		escapees = esc_request.get(class_object=SBUser)
+        esc_request = wpapi.UserRequest()
+        esc_request.context = "edit"
+        esc_request.per_page = 50
+        esc_request.roles = ['former_team_member']
+        escapees = esc_request.get(class_object=SBUser)
+        chooseFace(wpapi,escapees)
 
-		cont_request = wpapi.UserRequest()
-		cont_request.context = "edit"
-		cont_request.per_page = 50
-		cont_request.roles = ['contributor']
-		contributors = cont_request.get(class_object=SBUser)
+        cont_request = wpapi.UserRequest()
+        cont_request.context = "edit"
+        cont_request.per_page = 50
+        cont_request.roles = ['contributor']
+        contributors = cont_request.get(class_object=SBUser)
 
-		sac_request = wpapi.UserRequest()
-		sac_request.context = "edit"
-		sac_request.per_page = 50
-		sac_request.roles = ['sac']
-		sac = sac_request.get(class_object=SBUser)
+        sac_request = wpapi.UserRequest()
+        sac_request.context = "edit"
+        sac_request.per_page = 50
+        sac_request.roles = ['sac']
+        sac = sac_request.get(class_object=SBUser)
+        chooseFace(wpapi,sac)
 
-		people_banner_media = wpapi.media(slug="sorghum_combine")
-		templateDict["banner_media"] = people_banner_media
+        people_banner_media = wpapi.media(slug="sorghum_combine")
+        templateDict["banner_media"] = people_banner_media
 
-		populate_footer_template(template_dictionary=templateDict, wp_api=wpapi, photos_to_credit=[people_banner_media])
+        populate_footer_template(template_dictionary=templateDict, wp_api=wpapi, photos_to_credit=[people_banner_media])
 
-	templateDict['leader'] = leader[0]
-	templateDict['team'] = team
-	templateDict['contributors'] = contributors
-	templateDict['sac'] = sac
-	templateDict['escapees'] = escapees
+    templateDict['leader'] = leader[0]
+    templateDict['team'] = team
+    templateDict['contributors'] = contributors
+    templateDict['sac'] = sac
+    templateDict['escapees'] = escapees
 
-	return render_template("people.html", **templateDict)
+    return render_template("people.html", **templateDict)
