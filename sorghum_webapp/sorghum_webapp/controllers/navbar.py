afd0dad2f686df3d669cb858cf2f89792efc9ca5
Andrew Olson <olson@cshl.edu>
2021-07-21 17:49:04 -0400

    added link to youtube videos

--- sorghum_webapp/sorghum_webapp/controllers/navbar.py
+++ sorghum_webapp/sorghum_webapp/controllers/navbar.py
@@ -20,6 +20,7 @@ def engage():
     menu = make_menu('Engage')
 #     add_link(menu, 'Research Notes', '/posts?categories=researchnote')
     add_link(menu, 'Training Materials', '/guides')
+    add_link(menu, 'Videos', 'https://www.youtube.com/channel/UCXpgZNk1JDIn0-7AaS4EBxQ')
 #     add_link(menu, 'Office Hours', '/office_hours')
     add_link(menu, 'Mailing List', '/mailing_list')
     add_link(menu, 'Contact Us', '/contact')
